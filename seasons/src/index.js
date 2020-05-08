3c5f4f9aac421f02059b1afbc3524d81c1950678
Sagi Amar <sagitimamar@gmail.com>
2020-05-08 23:38:40 +0300

    finished first react app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,41 +1,48 @@
 import React from 'react';
-
+import Spinner from './spinner';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-	constructor (props) {
-		super(props);
+	state = { lat: null, errorMessage: ''};
 
-		this.state = { lat: null, errorMessage: '' };
-
-	
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-		position => {
-			this.setState({ lat: position.coords.latitude});
-		},
-
-		err => {
-			this.setState({ errorMessage: err.message })
-			}
+		position => this.setState({ lat: position.coords.latitude}),
+		err => this.setState({ errorMessage: err.message })
 		);
 
 	}
 
-
-	render() {
+renderContent() {
 		if (this.state.errorMessage && this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div>
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>
+			return <SeasonDisplay lat={this.state.lat} />
 		}
 
-		return <div>Loading!</div>;
+		return <Spinner message="Please accept location request"/>;
 	}
+
+
+//below this, is a helper message that takes in the render content as a whole and adds a "border"
+	render() {
+		return (
+			<div className="border red">
+				{this.renderContent()}
+			</div>
+			);
+
+	}
+
 }
+
+
+
+
 
 
 
